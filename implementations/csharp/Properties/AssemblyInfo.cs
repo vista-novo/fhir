80ff69270b323c6e9ea152a208a07c6102f9c9da
ewoutkramer <ewoutkramer@2f0db536-2c49-4257-a3fa-e771ed206c19>
2012-06-01 08:50:20 +0000

    - Renamed directories and namespaces to conform to MS guidelines
    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk@286 2f0db536-2c49-4257-a3fa-e771ed206c19

--- implementations/csharp/Properties/AssemblyInfo.cs
+++ implementations/csharp/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FHIR")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("HL7.Fhir.Instance")]
+[assembly: AssemblyDescription("FHIR Resource model, parsers and support for C#")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("FHIR")]
