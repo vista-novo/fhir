1d39a593db35121b60988fd3595b236018e76f51
grahameg <grahameg@2f0db536-2c49-4257-a3fa-e771ed206c19>
2012-06-09 12:54:54 +0000

    git-svn-id: http://gforge.hl7.org/svn/fhir/trunk@320 2f0db536-2c49-4257-a3fa-e771ed206c19

--- tools/java/org.hl7.fhir.instance/src/org/hl7/fhir/instance/model/Factory.java
+++ tools/java/org.hl7.fhir.instance/src/org/hl7/fhir/instance/model/Factory.java
@@ -1,25 +1,63 @@
 package org.hl7.fhir.instance.model;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/*
+Copyright (c) 2011-2012, HL7, Inc
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice, this 
+   list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, 
+   this list of conditions and the following disclaimer in the documentation 
+   and/or other materials provided with the distribution.
+ * Neither the name of HL7 nor the names of its contributors may be used to 
+   endorse or promote products derived from this software without specific 
+   prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+
+
 public class Factory {
 
-  public static Id newId(String metadata) {
-    // TODO Auto-generated method stub
-    return null;
+  public static Id newId(String value) {
+    Id res = new Id();
+    res.setValue(value);
+    return res;
   }
 
-  public static String_ newString_(String metadata) {
-    // TODO Auto-generated method stub
-    return null;
+  public static String_ newString_(String value) {
+    String_ res = new String_();
+    res.setValue(value);
+    return res;
   }
 
-  public static Uri newUri(String metadata) {
-    // TODO Auto-generated method stub
-    return null;
+  public static Uri newUri(String value) throws URISyntaxException {
+    Uri res = new Uri();
+    res.setValue(new URI(value));
+    return res;
   }
 
-  public static DateTime newDateTime(String metadata) {
-    // TODO Auto-generated method stub
-    return null;
+  public static DateTime newDateTime(String value) {
+    DateTime res = new DateTime();
+    res.setValue(value);
+    return res;
   }
 
 }
